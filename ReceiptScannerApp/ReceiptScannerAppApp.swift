9b9b663bab3775b030834b205b3972bb55b0a7f9
boubelpi <113375173+boubelpi@users.noreply.github.com>
2022-12-10 03:43:48 +0100

    Initial Commit

---
+++ ReceiptScannerApp/ReceiptScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptScannerAppApp.swift
+//  ReceiptScannerApp
+//
+//  Created by Pavel on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
